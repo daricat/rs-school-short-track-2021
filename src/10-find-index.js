4d8cf11184fd8f11540399c3d73e89c332ecd2d0
daricat <darina.talk@gmail.com>
2021-04-07 15:00:18 +0300

    feat: add solve to 10-find-index

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,23 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  const medium = (left, right) => Math.floor((left + right) / 2);
+
+  const find = (left, middle, right) => {
+    switch (true) {
+      case array[middle] === value:
+        return middle;
+      case array[middle] > value:
+        return find(left, medium(left, middle), middle);
+      case array[middle] < value:
+        return find(middle, medium(middle, right), right);
+      default:
+        return 1;
+    }
+  };
+
+  return find(0, medium(0, array.length), array.length);
 }
 
 module.exports = findIndex;
