91ee1aff509ace638d851c957a7e3a8782d9deda
daricat <darina.talk@gmail.com>
2021-04-09 03:30:55 +0300

    feat: add solve to 07-file-names

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,32 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(nam) {
+  const rename = (names) => {
+    const namesCopy = [...names];
+    const unique = () => {
+      [...new Set(namesCopy)]
+        .map((uniqueName) => namesCopy.reduce((indexArray, name, i) => {
+          if (name === uniqueName) indexArray.push(i);
+          return indexArray;
+        }, []))
+        .map((indexArray) => indexArray.forEach((index, i) => {
+          if (i !== 0) namesCopy[index] = `${namesCopy[index]}(${i})`;
+        }));
+    };
+    unique();
+    return namesCopy;
+  };
+
+  const firstRenamingFiles = rename(nam);
+
+  const checkDuplicates = (arr) => {
+    if (arr.length === [...new Set(arr)].length) return arr;
+    const renamingFiles = rename(arr);
+    return checkDuplicates(renamingFiles);
+  };
+
+  return checkDuplicates(firstRenamingFiles);
 }
 
 module.exports = renameFiles;
